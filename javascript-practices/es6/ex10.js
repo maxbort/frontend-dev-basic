b736bc8635320e07bd7433c860f1cb4df86c5d4f
CHOI HyoJun <cjfgh2449@gmail.com>
2024-07-14 15:25:06 +0900

    Parenthesis-less function

--- javascript-practices/es6/ex10.js
+++ javascript-practices/es6/ex10.js
@@ -1,6 +1,7 @@
 // Parenthesis-less function & template literal
 
-div = function(s, ...v) {
+div = function(s, ...v) { //s: 템플릿 리터럴에서 고정된 문자열 부분이 배열로 전달됩니다.
+                            // ...v: 템플릿 리터럴에서 변수로 삽입된 값들이 배열로 전달됩니다.
     console.log(s, v);
     return function() {
         return <div style={{}}></div>;
